c658bc08191b65e85c50f559cdf65d522d87476b
bocai <woaichibocai_hxb@163.com>
2021-08-14 22:57:34 +0800

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -26,8 +26,28 @@ module.exports = async function(deployer, network, accounts) {
     // let erc20Token = await deployProxy(TestERC20Token, [], {deployer});
     // console.log('TestERC20Token: ', erc20Token.address);
 
-    // let suterERC20 = await deployProxy(SuterERC20, [erc20Token.address, transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
-    // await suterERC20.setUnit("10000000000000000000000", {from: accounts[0]});
-    // console.log('SuterERC20: ', suterERC20.address);
+    // BUSD
+    let suterBusd = await deployProxy(SuterERC20, ['0x03f85f25e16fF2c6F39A46e096052381D92CeE19', transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
+    await suterBusd.setUnit("1000000000000000000", {from: accounts[0]});
+    console.log('suterBusd: ', suterBusd.address);
 
+    // cake
+    let suterCake = await deployProxy(SuterERC20, ['0x5B5bC8624F595136CdC4593606883c60A2150aF2', transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
+    await suterCake.setUnit("1000000000000000000", {from: accounts[0]});
+    console.log('suterBusd: ', suterCake.address);
+
+    // cake
+    let suterBake = await deployProxy(SuterERC20, ['0x5125c6871e5B9c64C88BA9c1CF6027BB43D3e4c1', transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
+    await suterBake.setUnit("1000000000000000000", {from: accounts[0]});
+    console.log('suterBake: ', suterBake.address);
+
+    // suter
+    let suterSuter = await deployProxy(SuterERC20, ['0x31A12B827b4FD44ea0d81ddaCa57f691EEe40362', transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
+    await suterSuter.setUnit("1000000000000000000", {from: accounts[0]});
+    console.log('suterSuter: ', suterSuter.address);
+
+    // xsuter
+    let suterxSuter = await deployProxy(SuterERC20, ['0x23538e1B238cb927fdF2d8A5d7597432De98274E', transferVerifier.address, burnVerifier.address], {deployer, initializer: 'initializeSuterERC20'});
+    await suterxSuter.setUnit("1000000000000000000", {from: accounts[0]});
+    console.log('suterxSuter: ', suterxSuter.address);
 };
--- migrations/3_upgrade_contracts.js
+++ migrations/3_upgrade_contracts.js
@@ -9,23 +9,23 @@ const SuterERC20 = artifacts.require("SuterERC20");
 const CheckSuter = artifacts.require("CheckSuter");
 
 module.exports = async function(deployer, network, accounts) {
-    let ipVerifierExisting = await InnerProductVerifier.deployed();
-    let ipVerifier = await upgradeProxy(ipVerifierExisting.address, InnerProductVerifier, {deployer});
-    console.log('InnerProductVerifier: ', ipVerifier.address);
+    // let ipVerifierExisting = await InnerProductVerifier.deployed();
+    // let ipVerifier = await upgradeProxy(ipVerifierExisting.address, InnerProductVerifier, {deployer});
+    // console.log('InnerProductVerifier: ', ipVerifier.address);
 
-    let burnVerifierExisting = await BurnVerifier.deployed();
-    let burnVerifier = await upgradeProxy(burnVerifierExisting.address, BurnVerifier, {deployer});
-    console.log('BurnVerifier: ', burnVerifier.address);
+    // let burnVerifierExisting = await BurnVerifier.deployed();
+    // let burnVerifier = await upgradeProxy(burnVerifierExisting.address, BurnVerifier, {deployer});
+    // console.log('BurnVerifier: ', burnVerifier.address);
 
-    let transferVerifierExisting = await TransferVerifier.deployed();
-    let transferVerifier = await upgradeProxy(transferVerifierExisting.address, TransferVerifier, {deployer});
-    console.log('TransferVerifier: ', transferVerifier.address);
+    // let transferVerifierExisting = await TransferVerifier.deployed();
+    // let transferVerifier = await upgradeProxy(transferVerifierExisting.address, TransferVerifier, {deployer});
+    // console.log('TransferVerifier: ', transferVerifier.address);
 
-    let suterETHExisting = await SuterETH.deployed();
-    let suterETH = await upgradeProxy(suterETHExisting.address, SuterETH, {deployer});
-    console.log('SuterETH: ', suterETH.address);
+    // let suterETHExisting = await SuterETH.deployed();
+    // let suterETH = await upgradeProxy(suterETHExisting.address, SuterETH, {deployer});
+    // console.log('SuterETH: ', suterETH.address);
 
-    let suterERC20Existing = await SuterERC20.deployed();
-    let suterERC20 = await upgradeProxy(suterERC20Existing.address, SuterERC20, {deployer});
-    console.log('SuterERC20: ', suterERC20.address);
+    // let suterERC20Existing = await SuterERC20.deployed();
+    // let suterERC20 = await upgradeProxy(suterERC20Existing.address, SuterERC20, {deployer});
+    // console.log('SuterERC20: ', suterERC20.address);
 };
